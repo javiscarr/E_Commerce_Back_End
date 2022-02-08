24b28568bb3b04d3807804d047454d7ffd5c769b
Doonywoony <javismcarr@gmail.com>
2022-02-07 21:38:02 -0600

    index.js created

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const categoryRoutes = require("./category_routes");
+const productRoutes = require("./product_routes");
+const tagRoutes = require("./tag_routes");
+
+router.use("/categories", categoryRoutes);
+router.use("/products", productRoutes);
+router.use("/tags", tagRoutes);
+
+module.exports = router;
